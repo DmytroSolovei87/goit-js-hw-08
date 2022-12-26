038c0e26ab46ff25eb5b56604cae9c3357a856b9
DmytroSolovei87 <dsolovey87@gmail.com>
2022-12-26 22:30:13 +0200

    repair video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+'use strict';
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+function playedTime(data) {
+  const { seconds } = data;
+  localStorage.setItem(LOCAL_STORAGE_KEY, seconds);
+}
+
+const throttledPlayedTime = throttle(playedTime, 1000);
+
+player.on('timeupdate', throttledPlayedTime);
+
+const currentTime = localStorage.getItem(LOCAL_STORAGE_KEY);
+
+// player.setCurrentTime(currentTime);
+
+player.setCurrentTime(currentTime).catch(function (error) {
+  switch (error.name) {
+    case 'RangeError':
+      // the time was less than 0 or greater than the video’s duration
+      break;
+  }
+});
